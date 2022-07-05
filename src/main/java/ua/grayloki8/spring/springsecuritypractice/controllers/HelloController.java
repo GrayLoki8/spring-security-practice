a7c7b029add7a490c305163f95160b0eb220d90d
Gray Loki <skuc090@gmail.com>
2022-07-05 18:53:21 +0300

    add properties

---
+++ src/main/java/ua/grayloki8/spring/springsecuritypractice/controllers/HelloController.java
@@ -0,0 +1,11 @@
+package ua.grayloki8.spring.springsecuritypractice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
---
+++ src/test/java/ua/grayloki8/spring/springsecuritypractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.grayloki8.spring.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
