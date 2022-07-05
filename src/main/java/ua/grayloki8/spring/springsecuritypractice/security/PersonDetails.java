a8dfaf08d8474d8a556c874f31bf9af2c5c957fa
Gray Loki <skuc090@gmail.com>
2022-07-05 19:30:35 +0300

    create security class PersonDetails

---
+++ src/main/java/ua/grayloki8/spring/springsecuritypractice/security/PersonDetails.java
@@ -0,0 +1,50 @@
+package ua.grayloki8.spring.springsecuritypractice.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ua.grayloki8.spring.springsecuritypractice.models.User;
+
+import java.util.Collection;
+
+public class PersonDetails implements UserDetails {
+    private final User person;
+
+    public PersonDetails(User person) {
+        this.person = person;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.person.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.person.getUserName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
