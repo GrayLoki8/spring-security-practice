4b3dea0c76f78969da577dcc3c3e57f6d1740c77
Gray Loki <skuc090@gmail.com>
2022-07-06 16:25:22 +0300

    add custom login page

--- src/main/java/ua/grayloki8/spring/springsecuritypractice/security/SecurityConfig.java
+++ src/main/java/ua/grayloki8/spring/springsecuritypractice/security/SecurityConfig.java
@@ -1,20 +1,45 @@
 package ua.grayloki8.spring.springsecuritypractice.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ua.grayloki8.spring.springsecuritypractice.services.PersonDetailsService;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProvider authProvider;
-
-    public SecurityConfig(AuthProvider authProvider) {
-        this.authProvider = authProvider;
+    private final PersonDetailsService personDetailsService;
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-        System.out.println("from sec con");
-        auth.authenticationProvider(authProvider);;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //config Spring Security
+        //config authorizationn
+
+        http.csrf().disable().authorizeRequests().antMatchers("/auth/login","/error").permitAll().
+                anyRequest().authenticated().and().
+                formLogin().loginPage("/auth/login").
+                loginProcessingUrl("/process_login").
+        defaultSuccessUrl("/hello",true).
+        failureUrl("/auth/login?error");
+    }
+
+
+
+    //Configure authentication
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);;
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
